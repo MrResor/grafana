cd403abbf125faaec41579f7cfcb629f17ac5a37
Mr.Resor <jakusienko@gmail.com>
2025-06-27 17:08:42 +0000

    add test endpoint declaration

---
+++ src/api/project/index.mjs
@@ -0,0 +1,13 @@
+import express from 'express'
+import fs from 'fs'
+
+const project = express.Router()
+
+project.get('/api/hello', (_, res) => {
+  const result = fs.readdirSync('./logs', { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+  res.status(200).json({ result })
+})
+
+export { project }
